363613e633b8e240c9d96a90c6730ec8c32b5814
ChrisLoading <tsunghaochou@gmail.com>
2023-09-08 15:50:30 +0800

    update money

--- money/intent/Loki_money.py
+++ money/intent/Loki_money.py
@@ -556,14 +556,14 @@ def getResult(inputSTR, utterance, args, resultDICT, refDICT):
 
     if utterance == "借了[對方]好幾[十萬]":
         if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
+            resultDICT["response"] = getResponse(utterance, args).format(*args)
         else:
             # write your code here
             pass
 
     if utterance == "借了[對方]幾[十萬]":
         if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
+            resultDICT["response"] = getResponse(utterance, args).format(*args)
         else:
             # write your code here
             pass
@@ -626,21 +626,21 @@ def getResult(inputSTR, utterance, args, resultDICT, refDICT):
 
     if utterance == "月入[只有][2萬]":
         if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
+            resultDICT["response"] = getResponse(utterance, args).format(*args)
         else:
             # write your code here
             pass
 
     if utterance == "月收[只有][2萬多]":
         if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
+            resultDICT["response"] = getResponse(utterance, args).format(*args)
         else:
             # write your code here
             pass
 
     if utterance == "月收入[只有][2萬多]":
         if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
+            resultDICT["response"] = getResponse(utterance, args).format(*args)
         else:
             # write your code here
             pass
@@ -654,7 +654,7 @@ def getResult(inputSTR, utterance, args, resultDICT, refDICT):
 
     if utterance == "每個月[只有][2萬多]的[薪水]":
         if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
+            resultDICT["response"] = getResponse(utterance, args).format(*args)
         else:
             # write your code here
             pass
